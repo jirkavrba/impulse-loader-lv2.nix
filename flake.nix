e72f0c2f976783907584f6f674fe64281fd3bd07
Jiří Vrba <jiri@vrba.dev>
2026-03-07 07:34:03 +0100

    Add devshell + app for directly running carla with preconfigured IR

--- flake.nix
+++ flake.nix
@@ -3,56 +3,68 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =  { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        plugin = pkgs.stdenv.mkDerivation rec {
+       	  pname = "impulseloader-lv2";
+       	  version = "0.4";
+
+       	  src = pkgs.fetchFromGitHub {
+       	    owner = "brummer10";
+       	    repo = "ImpulseLoader";
+       	    tag = "v${version}";
+       	    hash = "sha256-NdbhOzaUCPHAD9RfGcHEuFxAZlQ4+hf7P3SF7hAd+UA=";
+       	    fetchSubmodules = true;
+       	  };
+
+       	  nativeBuildInputs = with pkgs; [
+       	    pkg-config
+       	  ];
+
+       	  buildInputs = with pkgs; [
+       	    cairo
+            libsndfile
+            libX11
+            lv2
+            fftw
+            fftwFloat
+       	  ];
+
+          installPhase = ''
+            mkdir -p $out/lib/lv2
+            cp -r bin/ImpulseLoader.lv2 $out/lib/lv2
+          '';
+        };
+
+        wiring = ./impulse-loader.carxp;
     in
-    {
-     	packages.${system}.default = pkgs.stdenv.mkDerivation rec {
-     	  pname = "impulseloader-lv2";
-     	  version = "0.4";
+      {
+        packages.default = plugin;
 
-     	  src = pkgs.fetchFromGitHub {
-     	    owner = "brummer10";
-     	    repo = "ImpulseLoader";
-     	    tag = "v${version}";
-     	    hash = "sha256-NdbhOzaUCPHAD9RfGcHEuFxAZlQ4+hf7P3SF7hAd+UA=";
-     	    fetchSubmodules = true;
-     	  };
+        devShells.default = pkgs.mkShell {
+          packages = [
+            plugin
+            pkgs.carla
+          ];
 
-     	  nativeBuildInputs = with pkgs; [
-     	    pkg-config
-     	  ];
+          shellHook = ''
+            export LV2_PATH="${plugin}/lib/lv2:$LV2_PATH"
+          '';
+        };
 
-     	  buildInputs = with pkgs; [
-     	    cairo
-          libsndfile
-          libX11
-          lv2
-          fftw
-          fftwFloat
-     	  ];
+        apps.default = flake-utils.lib.mkApp {
+          drv = pkgs.writeShellScriptBin "carla-ir" ''
+            export LV2_PATH=${plugin}/lib/lv2;
+            exec ${pkgs.carla}/bin/carla --load ${wiring}
+          '';
+        };
+      }
 
-        buildPhase = ''
-          FFTW3F_LIB=${pkgs.fftwFloat.out}/lib
-
-          sed -i "s/-lfftw3f/-L$FFTW3F -lfftw3f/g" makefile
-
-          export CXXFLAGS="$(pkg-config --cflags cairo lv2 fftw3)"
-          export LDFLAGS="$(pkg-config --libs cairo lv2 fftw3)"
-
-          make
-        '';
-
-        installPhase = ''
-          mkdir -p $out/lib/lv2
-          cp -r ./bin/ImpulseLoader.lv2 $out/lib/lv2
-        '';
-  	};
-	};
+  );
 }
